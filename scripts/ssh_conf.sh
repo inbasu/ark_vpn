7d23b44d07c1ba617216e953feeff66e790d60b2
1nbasu <just.me.juncy@gmail.com>
2026-03-30 22:50:14 +0300

    modified:   scripts/ssh_conf.sh

--- scripts/ssh_conf.sh
+++ scripts/ssh_conf.sh
@@ -1,8 +1,8 @@
 touch /etc/ssh/sshd_config.d/base.conf
 
-eche "PasswordAuthentication no" >> /etc/ssh/sshd_config.d/base.conf
-eche "PermitEmptyPasswords no" >> /etc/ssh/sshd_config.d/base.conf
-eche "PubkeyAuthentication yes" >> /etc/ssh/sshd_config.d/base.conf
-eche "PermitRootLogin prohibit-password" >> /etc/ssh/sshd_config.d/base.conf
+echo "PasswordAuthentication no" >> /etc/ssh/sshd_config.d/base.conf
+echo "PermitEmptyPasswords no" >> /etc/ssh/sshd_config.d/base.conf
+echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config.d/base.conf
+echo "PermitRootLogin prohibit-password" >> /etc/ssh/sshd_config.d/base.conf
 
 systemctl restart sshd
